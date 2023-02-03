91ad4fed2d4742d390820d3367c773663df1f72a
itsbellow <mateusbetatbello17@gmail.com>
2023-02-02 22:55:30 -0300

    last one

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ function App() {
 
   const titulo='Perguntas e Respotas Frequentes'
 
+  const [UrlDoVideoInicial, setUrlDoVideoInicial] = useState("https://www.youtube.com/embed/4PqECm1OA7U")
+
+  const [search, setSearch] = useState("")
+
   const Topo=()=>{
     return(
     <div className="container-fluid">
       <div className="row">
         <div className="col-7">
-          <h4 className="mt-3"><svg className="ml-3 bi bi-chat-right-text" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1H2zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12z"/>
-            <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
-            </svg> <strong>FAQ STARK</strong></h4>
+          <h4 className="mt-3 ml-3 d-flex align-items-center d-flex gap-2"><i class="material-symbols-outlined">quiz</i><strong>FAQ STARK</strong></h4>
           <h6 className="ml-3">{titulo}</h6>
         </div>
         <div class="col-5 m-auto">
@@ -26,15 +27,13 @@ function App() {
     )
   }
 
-  const [UrlDoVideoInicial, setUrlDoVideoInicial] = useState("https://www.youtube.com/embed/4PqECm1OA7U")
-
   const Inicio=()=>{
     return(
             <div className="accordion-item">
               <h2 className="accordion-header" id="flush-headingOne">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                <button className="accordion-button collapsed d-flex gap-2" type="button" data-bs-toggle="collapse"
                   data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                  Início
+                  <i className='material-icons'>home</i><strong>Início</strong>
                 </button>
               </h2>
               <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne"
@@ -49,9 +48,9 @@ function App() {
     return(
             <div className="accordion-item">
               <h2 className="accordion-header" id="flush-headingTwo">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                <button className="accordion-button collapsed d-flex gap-2" type="button" data-bs-toggle="collapse"
                   data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                  Combinações
+                  <i className='material-icons'>favorite</i><strong>Combinações</strong>
                 </button>
               </h2>
               <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo"
@@ -66,9 +65,9 @@ function App() {
     return(
             <div className="accordion-item">
               <h2 className="accordion-header" id="flush-headingThree">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                <button className="accordion-button collapsed d-flex gap-2" type="button" data-bs-toggle="collapse"
                   data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                  Originações
+                  <i className='material-icons'>filter_alt</i><strong>Originações</strong>
                 </button>
               </h2>
               <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree"
@@ -83,9 +82,9 @@ function App() {
     return(
             <div className="accordion-item">
               <h2 className="accordion-header" id="flush-headingFour">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                <button className="accordion-button collapsed d-flex gap-2" type="button" data-bs-toggle="collapse"
                   data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
-                  Clientes
+                  <i className='material-icons'>business</i><strong>Clientes</strong>
                 </button>
               </h2>
               <div id="flush-collapseFour" className="accordion-collapse collapse" aria-labelledby="flush-headingFour"
@@ -100,9 +99,9 @@ function App() {
     return(
             <div className="accordion-item">
               <h2 className="accordion-header" id="flush-headingFive">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                <button className="accordion-button collapsed d-flex gap-2" type="button" data-bs-toggle="collapse"
                   data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
-                  Investidores
+                  <i className='material-icons'>account_balance</i><strong>Investidores</strong>
                 </button>
               </h2>
               <div id="flush-collapseFive" className="accordion-collapse collapse" aria-labelledby="flush-headingFive"
@@ -117,9 +116,9 @@ function App() {
     return(
             <div className="accordion-item">
               <h2 className="accordion-header" id="flush-headingSix">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                <button className="accordion-button collapsed d-flex gap-2" type="button" data-bs-toggle="collapse"
                   data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
-                  Agentes
+                  <i className='material-icons'>groups</i><strong>Agentes</strong>
                 </button>
               </h2>
               <div id="flush-collapseSix" className="accordion-collapse collapse" aria-labelledby="flush-headingSix"
@@ -134,9 +133,9 @@ function App() {
     return(
             <div className="accordion-item">
               <h2 className="accordion-header" id="flush-headingSeven">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                <button className="accordion-button collapsed d-flex gap-2" type="button" data-bs-toggle="collapse"
                   data-bs-target="#flush-collapseSeven" aria-expanded="false" aria-controls="flush-collapseSeven">
-                  Ferramentas
+                  <i className='material-icons'>construction</i><strong>Ferramentas</strong>
                 </button>
               </h2>
               <div id="flush-collapseSeven" className="accordion-collapse collapse" aria-labelledby="flush-headingSeven"
@@ -164,7 +163,7 @@ function App() {
     <div className="container-fluid">
       <div className="row">
         <div className="col-3">
-          <div className="ml-2 border accordion accordion-flush rounded shadow-lg p-3 mb-5 bg-white rounded" id="accordionFlushExample">
+          <div className="ml-3 border accordion accordion-flush rounded shadow-lg p-3 mb-5 bg-white rounded" id="accordionFlushExample">
             <Inicio/>
             <Combinações/>
             <Originações/>
